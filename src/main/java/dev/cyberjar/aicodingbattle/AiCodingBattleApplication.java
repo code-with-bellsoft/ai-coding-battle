3902cc6ad434a8ac8bfb06ab757fa7683ec63d2e
Catherine Edelveis <ekaterina.edelveis@gmail.com>
2026-02-27 16:09:28 +0300

    first commit

---
+++ src/main/java/dev/cyberjar/aicodingbattle/AiCodingBattleApplication.java
@@ -0,0 +1,13 @@
+package dev.cyberjar.aicodingbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodingBattleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodingBattleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cyberjar/aicodingbattle/AiCodingBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cyberjar.aicodingbattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodingBattleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
